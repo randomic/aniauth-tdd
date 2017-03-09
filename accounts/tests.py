74a5cad21fb726384ab53f2ca9b711cc8298bfb9
randomic <j.a.d.mcmillan@gmail.com>
2017-03-09 15:32:28 +0000

    Add docstrings to unit test

--- accounts/tests.py
+++ accounts/tests.py
@@ -5,6 +5,12 @@ from django.test import TestCase
 
 
 class WelcomePageTest(TestCase):
+    """Tests relating to the welcome_page view.
+
+    """
     def test_uses_welcome_template(self):
+        """The root url should response with the welcome page template.
+
+        """
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'accounts/welcome.html')
