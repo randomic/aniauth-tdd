202823cc425c7e341524b3af98186f0642dc9437
randomic <j.a.d.mcmillan@gmail.com>
2017-03-09 16:17:52 +0000

    Unit test user model

--- accounts/tests.py
+++ accounts/tests.py
@@ -2,6 +2,7 @@
 
 """
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
 
 class WelcomePageTest(TestCase):
@@ -14,3 +15,22 @@ class WelcomePageTest(TestCase):
         """
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'accounts/welcome.html')
+
+
+class UserModelTest(TestCase):
+    """Tests for passwordless user model.
+
+    """
+    def test_user_valid_with_only_email(self):
+        """Should not raise if the user model is happy with email only
+
+        """
+        user = get_user_model()(email='newvisitor@example.com')
+        user.full_clean()
+
+    def test_users_are_authenticated(self):
+        """User objects should be authenticated for views/templates.
+
+        """
+        user = get_user_model()()
+        self.assertTrue(user.is_authenticated())
