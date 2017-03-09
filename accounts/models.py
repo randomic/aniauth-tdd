2783def3b15161536b6ec7ed480972f0f51ae5c3
randomic <j.a.d.mcmillan@gmail.com>
2017-03-09 16:18:19 +0000

    Implement custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
-# Create your models here.
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
+
+    USERNAME_FIELD = 'email'
+
+    def __init__(self, *args, **kwargs):
+        super(User, self).__init__(*args, **kwargs)
+
+        self.set_unusable_password()
