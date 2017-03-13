580eb57f68a8501cae925f747e94a8edb2c6a028
randomic <j.a.d.mcmillan@gmail.com>
2017-03-13 14:09:19 +0000

    Add unit test for LoginToken model

--- accounts/tests.py
+++ accounts/tests.py
@@ -4,6 +4,11 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+from accounts.models import LoginToken
+
+
+TEST_EMAIL = 'newvisitor@example.com'
+
 
 class WelcomePageTest(TestCase):
     """Tests relating to the welcome_page view.
@@ -22,10 +27,10 @@ class UserModelTest(TestCase):
 
     """
     def test_user_valid_with_only_email(self):
-        """Should not raise if the user model is happy with email only
+        """Should not raise if the user model is happy with email only.
 
         """
-        user = get_user_model()(email='newvisitor@example.com')
+        user = get_user_model()(email=TEST_EMAIL)
         user.full_clean()
 
     def test_users_are_authenticated(self):
@@ -34,3 +39,16 @@ class UserModelTest(TestCase):
         """
         user = get_user_model()()
         self.assertTrue(user.is_authenticated())
+
+
+class TokenModelTest(TestCase):
+    """Tests for login token model.
+
+    """
+    def test_unique_tokens_generated(self):
+        """Two tokens generated should be unique.
+
+        """
+        token1 = LoginToken(TEST_EMAIL)
+        token2 = LoginToken(TEST_EMAIL)
+        self.assertNotEqual(token1, token2)
