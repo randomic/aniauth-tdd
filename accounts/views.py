0dea3128958fe2dca900ee5a14989e21fd0b6c2e
randomic <j.a.d.mcmillan@gmail.com>
2017-03-16 23:26:28 +0000

    Implement basic view

--- accounts/views.py
+++ accounts/views.py
@@ -6,3 +6,7 @@ from django.shortcuts import render
 
 def welcome_page(request):
     return render(request, 'accounts/welcome.html')
+
+
+def send_login_email(request):
+    return render(request, 'accounts/login_email_sent.html')
