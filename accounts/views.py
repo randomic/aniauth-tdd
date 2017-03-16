c1faca2b7101ef94e7cc9500dc653d1f7727109e
randomic <j.a.d.mcmillan@gmail.com>
2017-03-16 23:27:27 +0000

    Require POST method for the view

--- accounts/views.py
+++ accounts/views.py
@@ -2,11 +2,13 @@
 
 """
 from django.shortcuts import render
+from django.views.decorators.http import require_POST
 
 
 def welcome_page(request):
     return render(request, 'accounts/welcome.html')
 
 
+@require_POST
 def send_login_email(request):
     return render(request, 'accounts/login_email_sent.html')
