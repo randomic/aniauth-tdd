82700c4a5d11f01b971b8c031d8864cff2737f0e
randomic <j.a.d.mcmillan@gmail.com>
2017-03-20 21:25:52 +0000

    Add user manager to pass token tests

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,25 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser, PermissionsMixin)
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email)
+        )
+
+        user.save(using=self._db)
+        return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
+
+    objects = UserManager()
 
     USERNAME_FIELD = 'email'
 
