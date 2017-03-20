4a75d2b12fdaea392e3cf74eb1335b93c8aacdbd
randomic <j.a.d.mcmillan@gmail.com>
2017-03-20 21:24:47 +0000

    Tidy up UserModel unit test

--- accounts/tests/test_models.py
+++ accounts/tests/test_models.py
@@ -5,6 +5,7 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 
 
+USER = get_user_model()
 TEST_EMAIL = 'newvisitor@example.com'
 
 
@@ -16,12 +17,12 @@ class UserModelTest(TestCase):
         """Should not raise if the user model is happy with email only.
 
         """
-        user = get_user_model()(email=TEST_EMAIL)
+        user = USER(email=TEST_EMAIL)
         user.full_clean()
 
     def test_users_are_authenticated(self):
         """User objects should be authenticated for views/templates.
 
         """
-        user = get_user_model()()
+        user = USER()
         self.assertTrue(user.is_authenticated())
