067597b3a790f4528caa3a5161be13f2e4311fbd
randomic <j.a.d.mcmillan@gmail.com>
2017-03-21 17:28:17 +0000

    Add more loginform tests

--- accounts/tests/test_forms.py
+++ accounts/tests/test_forms.py
@@ -1,7 +1,10 @@
 """accounts app unittests for forms
 
 """
-from django.test import TestCase
+import re
+
+from django.test import TestCase, RequestFactory
+from django.core import mail
 
 from accounts.forms import LoginForm
 
@@ -10,10 +13,28 @@ class LoginFormTest(TestCase):
     """Tests the form which validates the email used for login.
 
     """
+    def setUp(self):
+        self.test_email = 'newvisitor@example.com'
+        self.request = RequestFactory().get('/')
+
     def test_valid_email_accepted(self):
-        form = LoginForm({'email': 'newvisitor@example.com'})
+        form = LoginForm({'email': self.test_email})
         self.assertTrue(form.is_valid())
 
     def test_invalid_email_declined(self):
         form = LoginForm({'email': 'invalidemail'})
         self.assertFalse(form.is_valid())
+
+    def test_can_send_email(self):
+        form = LoginForm({'email': self.test_email})
+        if form.is_valid():
+            form.save(self.request)
+        self.assertEqual(len(mail.outbox), 1)
+
+    def test_email_contains_link(self):
+        form = LoginForm({'email': self.test_email})
+        if form.is_valid():
+            form.save(self.request)
+        email = mail.outbox[0]
+        url_search = re.search(r'http://.+/.+$', email.body)
+        self.assertIsNotNone(url_search)
