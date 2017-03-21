7e0a23870cd763bdbd4d9fe4a9f866f98a50a2d4
randomic <j.a.d.mcmillan@gmail.com>
2017-03-21 16:09:17 +0000

    Modify login email view tests
    The send_login_email view should redirect to login_email_sent view on
    success.

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -3,7 +3,7 @@
 """
 from django.test import TestCase
 from django.core import mail
-from django.urls import reverse
+from django.urls import reverse_lazy
 
 
 class WelcomePageTest(TestCase):
@@ -23,15 +23,15 @@ class SendLoginEmailTest(TestCase):
 
     """
     def setUp(self):
-        self.url = reverse('send_login_email')
+        self.url = reverse_lazy('send_login_email')
         self.test_email = 'newvisitor@example.com'
 
-    def test_uses_emailsent_template(self):
-        """The send_login_email url responds with login_email_sent template.
+    def test_redirects_to_emailsent(self):
+        """The send_login_email url redirects to login_email_sent.
 
         """
         response = self.client.post(self.url, data={'email': self.test_email})
-        self.assertTemplateUsed(response, 'accounts/login_email_sent.html')
+        self.assertRedirects(response, reverse_lazy('login_email_sent'))
 
     def test_get_request_yields_405(self):
         """Accessing the view via get request is not allowed.
@@ -45,4 +45,16 @@ class SendLoginEmailTest(TestCase):
 
         """
         self.client.post(self.url, data={'email': self.test_email})
-        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(mail.outbox[0].to, [self.test_email])
+
+
+class LoginEmailSentTest(TestCase):
+    """Tests for the view which displays success message.
+
+    """
+    def test_uses_correct_template(self):
+        """The view should use the template which contains a success message.
+
+        """
+        response = self.client.get(reverse_lazy('login_email_sent'))
+        self.assertTemplateUsed(response, 'accounts/login_email_sent.html')
