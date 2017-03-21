7fd09bd791661ab0b12921dfd977591690d9c01a
randomic <j.a.d.mcmillan@gmail.com>
2017-03-21 16:30:13 +0000

    Add form tests for LoginForm

---
+++ accounts/tests/test_forms.py
@@ -0,0 +1,19 @@
+"""accounts app unittests for views
+
+"""
+from django.test import TestCase
+
+from accounts.forms import LoginForm
+
+
+class LoginFormTest(TestCase):
+    """Tests the form which validates the email used for login.
+
+    """
+    def test_valid_email_accepted(self):
+        form = LoginForm({'email': 'newvisitor@example.com'})
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_email_declined(self):
+        form = LoginForm({'email': 'invalidemail'})
+        self.assertFalse(form.is_valid())
