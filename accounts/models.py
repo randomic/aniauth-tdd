94beab28f1356ec4497b51f9f4f5fecca01776ec
randomic <j.a.d.mcmillan@gmail.com>
2017-04-20 16:02:23 +0100

    Simplify by using built-in User model
    Slightly modified to allow email-only user creation

--- accounts/models.py
+++ accounts/models.py
@@ -1,29 +1,10 @@
-from django.db import models
-from django.contrib.auth.models import (
-    BaseUserManager, AbstractBaseUser, PermissionsMixin)
+from django.contrib.auth.models import AbstractUser
 
 
-class UserManager(BaseUserManager):
-    def create_user(self, email):
-        if not email:
-            raise ValueError('Users must have an email address')
-
-        user = self.model(
-            email=self.normalize_email(email)
-        )
-
-        user.save(using=self._db)
-        return user
-
-
-class User(AbstractBaseUser, PermissionsMixin):
-    email = models.EmailField(unique=True)
-
-    objects = UserManager()
-
-    USERNAME_FIELD = 'email'
-
-    def __init__(self, *args, **kwargs):
-        super(User, self).__init__(*args, **kwargs)
-
-        self.set_unusable_password()
+class User(AbstractUser):
+    def full_clean(self, *args, **kwargs):
+        if not self.username:
+            self.username = self.email
+        if not self.password:
+            self.set_unusable_password()
+        super(User, self).full_clean(*args, **kwargs)
