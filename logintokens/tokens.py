23dac5f2e5be725eff7d329a6af399f6d80c59de
randomic <j.a.d.mcmillan@gmail.com>
2017-04-26 12:41:58 +0100

    Use last login of user in signed value

--- logintokens/tokens.py
+++ logintokens/tokens.py
@@ -3,8 +3,11 @@
 """
 import base64
 
-from django.core.signing import TimestampSigner, BadSignature
 from django.contrib.auth import get_user_model
+from django.core.signing import TimestampSigner, BadSignature
+
+
+USER = get_user_model()
 
 
 class LoginTokenGenerator:
@@ -15,12 +18,20 @@ class LoginTokenGenerator:
         self.signer = TimestampSigner(
             salt='aniauth-tdd.accounts.token.LoginTokenGenerator')
 
-    def make_token(self, email):
+    def make_token(self, username):
         """Return a login token for the provided email.
 
         """
+        try:
+            user = USER.objects.get(username=username)
+            login_timestamp = ('' if user.last_login is None
+                               else int(user.last_login.timestamp()))
+        except USER.DoesNotExist:
+            login_timestamp = ''
+
+        value = str('%s%s%s') % (username, self.signer.sep, login_timestamp)
         return base64.urlsafe_b64encode(
-            self.signer.sign(email).encode()
+            self.signer.sign(value).encode()
         ).decode()
 
     def consume_token(self, token, max_age=600):
@@ -28,8 +39,20 @@ class LoginTokenGenerator:
 
         """
         try:
-            return self.signer.unsign(
+            result = self.signer.unsign(
                 base64.urlsafe_b64decode(token.encode()), max_age
             )
         except (BadSignature, base64.binascii.Error):
             return None
+
+        username, login_timestamp = result.split(self.signer.sep)
+        try:
+            user = USER.objects.get(username=username)
+            user_login_timestamp = ('' if user.last_login is None
+                                    else int(user.last_login.timestamp()))
+            if user_login_timestamp == login_timestamp:
+                return username
+            else:
+                return None  # The user has logged in since this token was made
+        except USER.DoesNotExist:
+            return username
