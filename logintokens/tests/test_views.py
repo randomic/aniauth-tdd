d323ebb7567ba5a38700ad193c365780439040b5
randomic <j.a.d.mcmillan@gmail.com>
2017-04-27 18:47:14 +0100

    Add more token login view tests

--- logintokens/tests/test_views.py
+++ logintokens/tests/test_views.py
@@ -2,20 +2,60 @@
 
 """
 from django.test import TestCase
+from django.contrib.auth import get_user_model, get_user
 from django.core import mail
 from django.shortcuts import reverse
+
+from logintokens.tokens import default_token_generator
+
+
+USER = get_user_model()
 
 
 class TokenLoginViewTest(TestCase):
     """Tests for the view which verifies tokens and allows users to login.
 
     """
+    def setUp(self):
+        self.url = reverse('token_login')
+        self.generator = default_token_generator
+        self.new_username = 'tokenloginviewtest-newvisitor'
+        self.existing_user = USER._default_manager.create_user(
+            'tokenloginviewtest-existinguser')
+
     def test_redirects_to_home(self):
         """The view should eventually redirect to the homepage.
 
         """
-        response = self.client.get(reverse('token_login'))
+        response = self.client.get(self.url)
         self.assertRedirects(response, reverse('home'))
+
+    def test_valid_token(self):
+        """A valid token can be used to log in.
+
+        """
+        token = self.generator.make_token(self.existing_user.get_username())
+        self.client.get(self.url, data={'token': token})
+        self.assertTrue(get_user(self.client).is_authenticated)
+
+    def test_invalid_token(self):
+        """An invalid token cannot be used to log in.
+
+        """
+        self.client.get(self.url, data={'token': 'invalid_token'})
+        self.assertFalse(get_user(self.client).is_authenticated)
+
+    def test_new_user_can_login(self):
+        """A visitor without a user object can login with valid token.
+
+        """
+        with self.assertRaises(USER.DoesNotExist):
+            USER._default_manager.get_by_natural_key(self.new_username)
+
+        token = self.generator.make_token(self.new_username)
+        self.client.get(self.url, data={'token': token})
+        self.assertTrue(get_user(self.client).is_authenticated)
+        USER._default_manager.get_by_natural_key(self.new_username)
 
 
 class SendTokenViewTest(TestCase):
