5d2a4ac0e48d404a16b81d2f290be5ec13bdf8f1
randomic <j.a.d.mcmillan@gmail.com>
2017-04-27 19:40:49 +0100

    Update form to pass new test

--- logintokens/forms.py
+++ logintokens/forms.py
@@ -3,6 +3,7 @@
 """
 from django import forms
 from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UsernameField
 from django.core.mail import EmailMultiAlternatives
 from django.contrib.sites.shortcuts import get_current_site
 from django.urls import reverse_lazy
@@ -14,22 +15,30 @@ USER = get_user_model()
 
 
 class TokenLoginForm(forms.Form):
-    email = forms.EmailField(label="Email", max_length=254)
+    email = UsernameField(
+        max_length=254,
+        widget=forms.TextInput(attrs={'autofocus': True}),
+    )
 
-    def generate_login_link(self, email, request):
+    def generate_login_link(self, username, request):
         protocol = 'https' if request.is_secure() else 'http'
         domain = get_current_site(request).domain
         url = reverse_lazy('token_login')
-        token = default_token_generator.make_token(email)
+        token = default_token_generator.make_token(username)
         return '{}://{}{}?token={}'.format(protocol, domain, url, token)
 
     def save(self, request):
         """Generate a login token and send it to the email from the form.
 
         """
-        email = self.cleaned_data['email']
+        username = self.cleaned_data['email']
+        try:
+            user = USER._default_manager.get_by_natural_key(username)
+            email = getattr(user, USER.EMAIL_FIELD)
+        except USER.DoesNotExist:
+            email = username
         body = 'To complete the login process, simply click on this link: {}'
-        login_link = self.generate_login_link(email, request)
+        login_link = self.generate_login_link(username, request)
 
         email_message = EmailMultiAlternatives(
             'Your login link for ANIAuth',
