20cf0b8944bc79ea6fb3ef0eec7dfc810ac43118
randomic <j.a.d.mcmillan@gmail.com>
2017-04-27 19:40:22 +0100

    Add test for users logging in by username
    Mainly useful for users created by manage.py or using the admin
    interface.

--- logintokens/tests/test_forms.py
+++ logintokens/tests/test_forms.py
@@ -6,8 +6,12 @@ import re
 from django.test import TestCase, RequestFactory
 from django.core import mail
 from django.shortcuts import reverse
+from django.contrib.auth import get_user_model
 
 from logintokens.forms import TokenLoginForm
+
+
+USER = get_user_model()
 
 
 class LoginFormTest(TestCase):
@@ -15,16 +19,15 @@ class LoginFormTest(TestCase):
 
     """
     def setUp(self):
+        self.existing_user = USER._default_manager.create_user(
+            'loginformtest-existinguser',
+            'loginformtest-existinguser@example.com')
         self.test_email = 'newvisitor@example.com'
         self.request = RequestFactory().get('/')
 
     def test_valid_email_accepted(self):
         form = TokenLoginForm({'email': self.test_email})
         self.assertTrue(form.is_valid())
-
-    def test_invalid_email_declined(self):
-        form = TokenLoginForm({'email': 'invalidemail'})
-        self.assertFalse(form.is_valid())
 
     def test_can_send_email(self):
         form = TokenLoginForm({'email': self.test_email})
@@ -41,3 +44,15 @@ class LoginFormTest(TestCase):
         self.assertIsNotNone(url_search)
         url = url_search.group(0)
         self.assertIn(reverse('token_login'), url)
+
+    def test_normal_user(self):
+        """The link is sent to the user's email not to their username.
+
+        """
+        form = TokenLoginForm({'email': self.existing_user.get_username()})
+        if form.is_valid():
+            form.save(self.request)
+        email = mail.outbox[0]
+        self.assertEqual(
+            email.to,
+            [getattr(self.existing_user, USER.EMAIL_FIELD)])
