311ccab77fd59cebea26962edcf2f16f0aa25a6c
randomic <j.a.d.mcmillan@gmail.com>
2017-04-27 23:44:22 +0100

    Improve form validation to pass new tests

--- logintokens/forms.py
+++ logintokens/forms.py
@@ -2,6 +2,8 @@
 
 """
 from django import forms
+from django.core.validators import validate_email
+from django.core.exceptions import ValidationError
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UsernameField
 from django.core.mail import EmailMultiAlternatives
@@ -15,10 +17,22 @@ USER = get_user_model()
 
 
 class TokenLoginForm(forms.Form):
-    email = UsernameField(
+    email = UsernameField(  # For the majority of users it will be an email.
         max_length=254,
         widget=forms.TextInput(attrs={'autofocus': True}),
     )
+
+    def clean(self):
+        # Some users will have a username different from their email.
+        email = self.cleaned_data['email']
+        try:
+            user = USER._default_manager.get_by_natural_key(email)
+            clean_email = getattr(user, USER.EMAIL_FIELD)
+        except USER.DoesNotExist:
+            clean_email = email
+        validate_email(clean_email)
+        self.cleaned_data['clean_email'] = clean_email
+        return self.cleaned_data
 
     def generate_login_link(self, username, request):
         protocol = 'https' if request.is_secure() else 'http'
@@ -31,18 +45,15 @@ class TokenLoginForm(forms.Form):
         """Generate a login token and send it to the email from the form.
 
         """
-        username = self.cleaned_data['email']
-        try:
-            user = USER._default_manager.get_by_natural_key(username)
-            email = getattr(user, USER.EMAIL_FIELD)
-        except USER.DoesNotExist:
-            email = username
+        email = self.cleaned_data['email']
+        clean_email = self.cleaned_data['clean_email']
+
         body = 'To complete the login process, simply click on this link: {}'
-        login_link = self.generate_login_link(username, request)
+        login_link = self.generate_login_link(email, request)
 
         email_message = EmailMultiAlternatives(
             'Your login link for ANIAuth',
             body.format(login_link),
-            to=[email]
+            to=[clean_email]
         )
         email_message.send()
