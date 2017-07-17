0611f0ca617c3463a69e5bd627e821ae55c822ec
randomic <j.a.d.mcmillan@gmail.com>
2017-07-17 17:56:45 +0100

    Add docstrings to mocked time methods

--- logintokens/tests/util.py
+++ logintokens/tests/util.py
@@ -1,13 +1,25 @@
+"""utility functions for testing logintokens app
+
+"""
 from time import time
 
 
 class MockTime:
+    """Provide mocked time and sleep methods to simulate the passage of time.
+
+    """
     time_passed = 0.0
 
     def time(self):
+        """Return current time with a consistent offset.
+
+        """
         return time() + self.time_passed
 
     def sleep(self, seconds):
+        """Increase the offset to make it seem as though time has passed.
+
+        """
         self.time_passed += seconds
 
 mock_time = MockTime()
