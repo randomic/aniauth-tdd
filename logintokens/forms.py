cf007f89d6e14aae69e14577d2d570a1f3d63768
randomic <j.a.d.mcmillan@gmail.com>
2017-07-19 15:38:45 +0100

    Remove unused import

--- logintokens/forms.py
+++ logintokens/forms.py
@@ -3,7 +3,6 @@
 """
 from django import forms
 from django.core.validators import validate_email
-from django.core.exceptions import ValidationError
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UsernameField
 from django.core.mail import EmailMultiAlternatives
