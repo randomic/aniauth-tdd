53c1fa51b5824373f4359aca16ef1cc2f25a6595
randomic <j.a.d.mcmillan@gmail.com>
2017-08-26 11:57:52 +0100

    Implement some basic view tests

---
+++ evexml/tests/test_views.py
@@ -0,0 +1,50 @@
+import json
+
+from django.conf import settings
+from django.test import TestCase
+from django.shortcuts import reverse
+
+
+class AddAPIViewTest(TestCase):
+    """Tests for the view which displays the "Add API" form.
+
+    """
+    @classmethod
+    def setUpClass(cls):
+        super(AddAPIViewTest, cls).setUpClass()
+        data_dir = getattr(settings, 'DATA_DIR')
+        with data_dir.joinpath('conf', 'test_secrets.json').open() as handle:
+            secrets = json.load(handle)
+            cls.testkeys = secrets['apikeys']
+        cls.url = reverse('eveapi_add')
+
+    # Mask: 4294967295
+    def test_api_full_all(self):
+        """Ensure full and account-wide keypair is accepted.
+
+        """
+        keypair = self.testkeys['full']['all']
+        response = self.client.post(self.url, data={
+            'keyID': keypair['keyID'],
+            'vCode': keypair['vCode']}, follow=True)
+        self.assertContains(response, 'success')
+
+    def test_api_full_char_corp(self):
+        """Ensure full but corp character only keypair is rejected.
+
+        """
+        keypair = self.testkeys['full']['char_corp']
+        response = self.client.post(self.url, data={
+            'keyID': keypair['keyID'],
+            'vCode': keypair['vCode']}, follow=True)
+        self.assertContains(response, 'problem')
+
+    def test_api_full_char_noncorp(self):
+        """Ensure full but non-corp character only keypair is rejected.
+
+        """
+        keypair = self.testkeys['full']['char_noncorp']
+        response = self.client.post(self.url, data={
+            'keyID': keypair['keyID'],
+            'vCode': keypair['vCode']}, follow=True)
+        self.assertContains(response, 'problem')
