00a87fed616de3c6080e4826cc4636919b25e774
randomic <j.a.d.mcmillan@gmail.com>
2017-08-26 12:04:29 +0100

    Implement basic views

--- evexml/views.py
+++ evexml/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
 from django.views.generic.edit import FormView
+from django.views.generic.base import TemplateView
+from django.urls import reverse_lazy
+
+from evexml.forms import AddAPIForm
 
 
 class AddAPIView(FormView):
-    pass
+    template_name = 'evexml/eveapi_add.html'
+    form_class = AddAPIForm
+    success_url = reverse_lazy('eveapi_added')
+
+
+class AddedAPIView(TemplateView):
+    template_name = 'evexml/eveapi_added.html'
