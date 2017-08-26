9c668aa6e49a0b1395a2475981eb0eda7f4d0bd5
randomic <j.a.d.mcmillan@gmail.com>
2017-08-26 12:13:51 +0100

    Implement basic form tests

---
+++ evexml/tests/test_forms.py
@@ -0,0 +1,41 @@
+import json
+
+from django.test import TestCase
+from django.conf import settings
+
+from evexml.forms import AddAPIForm
+
+
+class AddAPIFormTest(TestCase):
+    """Tests for the view which displays the "Add API" form.
+
+    """
+    @classmethod
+    def setUpClass(cls):
+        super(AddAPIFormTest, cls).setUpClass()
+        data_dir = getattr(settings, 'DATA_DIR')
+        with data_dir.joinpath('conf', 'test_secrets.json').open() as handle:
+            secrets = json.load(handle)
+            cls.testkeys = secrets['apikeys']
+
+    # Mask: 4294967295
+    def test_api_full_all(self):
+        keypair = self.testkeys['full']['all']
+        form = AddAPIForm({
+            'keyID': keypair['keyID'],
+            'vCode': keypair['vCode']})
+        self.assertTrue(form.is_valid())
+
+    def test_api_full_char_corp(self):
+        keypair = self.testkeys['full']['char_corp']
+        form = AddAPIForm({
+            'keyID': keypair['keyID'],
+            'vCode': keypair['vCode']})
+        self.assertFalse(form.is_valid())
+
+    def test_api_full_char_noncorp(self):
+        keypair = self.testkeys['full']['char_noncorp']
+        form = AddAPIForm({
+            'keyID': keypair['keyID'],
+            'vCode': keypair['vCode']})
+        self.assertFalse(form.is_valid())
