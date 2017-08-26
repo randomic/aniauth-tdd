4b7e77b51318522db03f0b8230e9f7400cb8a312
randomic <j.a.d.mcmillan@gmail.com>
2017-08-26 16:38:47 +0100

    Add remaining test api keys to view tests

--- evexml/tests/test_views.py
+++ evexml/tests/test_views.py
@@ -1,3 +1,6 @@
+"""evexml app unittests for views
+
+"""
 import json
 
 from django.conf import settings
@@ -53,6 +56,99 @@ class AddAPIViewTest(TestCase):
 
         """
         keypair = self.testkeys['full']['char_noncorp']
+        response = self.client.post(self.url, data={
+            'key_id': keypair['key_id'],
+            'v_code': keypair['v_code']}, follow=True)
+        self.assertContains(response, 'problem')
+
+    # Mask: 4294901631
+    def test_api_partial_all(self):
+        """Ensure partial and account-wide keypair is rejected.
+
+        """
+        keypair = self.testkeys['partial']['all']
+        response = self.client.post(self.url, data={
+            'key_id': keypair['key_id'],
+            'v_code': keypair['v_code']}, follow=True)
+        self.assertContains(response, 'problem')
+
+    def test_api_partial_char_corp(self):
+        """Ensure partial and corp character only keypair is rejected.
+
+        """
+        keypair = self.testkeys['partial']['char_corp']
+        response = self.client.post(self.url, data={
+            'key_id': keypair['key_id'],
+            'v_code': keypair['v_code']}, follow=True)
+        self.assertContains(response, 'problem')
+
+    def test_api_partial_char_noncorp(self):
+        """Ensure partial and non-corp character only keypair is rejected.
+
+        """
+        keypair = self.testkeys['partial']['char_noncorp']
+        response = self.client.post(self.url, data={
+            'key_id': keypair['key_id'],
+            'v_code': keypair['v_code']}, follow=True)
+        self.assertContains(response, 'problem')
+
+    # Mask: 0
+    def test_api_blank_all(self):
+        """Ensure blank and account-wide keypair is rejected.
+
+        """
+        keypair = self.testkeys['blank']['all']
+        response = self.client.post(self.url, data={
+            'key_id': keypair['key_id'],
+            'v_code': keypair['v_code']}, follow=True)
+        self.assertContains(response, 'problem')
+
+    def test_api_blank_char_corp(self):
+        """Ensure blank and corp character only keypair is rejected.
+
+        """
+        keypair = self.testkeys['blank']['char_corp']
+        response = self.client.post(self.url, data={
+            'key_id': keypair['key_id'],
+            'v_code': keypair['v_code']}, follow=True)
+        self.assertContains(response, 'problem')
+
+    def test_api_blank_char_noncorp(self):
+        """Ensure full but non-corp character only keypair is rejected.
+
+        """
+        keypair = self.testkeys['blank']['char_noncorp']
+        response = self.client.post(self.url, data={
+            'key_id': keypair['key_id'],
+            'v_code': keypair['v_code']}, follow=True)
+        self.assertContains(response, 'problem')
+
+    # Expires
+    def test_api_expires_all(self):
+        """Ensure full and account-wide but expiring keypair is rejected.
+
+        """
+        keypair = self.testkeys['full_expires']['all']
+        response = self.client.post(self.url, data={
+            'key_id': keypair['key_id'],
+            'v_code': keypair['v_code']}, follow=True)
+        self.assertContains(response, 'problem')
+
+    def test_api_expires_char_corp(self):
+        """Ensure full but corp character only, expiring keypair is rejected.
+
+        """
+        keypair = self.testkeys['full_expires']['char_corp']
+        response = self.client.post(self.url, data={
+            'key_id': keypair['key_id'],
+            'v_code': keypair['v_code']}, follow=True)
+        self.assertContains(response, 'problem')
+
+    def test_api_expires_char_noncorp(self):
+        """Ensure full but non-corp character, expiring keypair is rejected.
+
+        """
+        keypair = self.testkeys['full_expires']['char_noncorp']
         response = self.client.post(self.url, data={
             'key_id': keypair['key_id'],
             'v_code': keypair['v_code']}, follow=True)
