20afc1dae1710605d36a6d4764d7edb99196e1c6
randomic <j.a.d.mcmillan@gmail.com>
2017-08-27 00:20:15 +0100

    Finalise AddAPIForm to pass all tests

--- evexml/forms.py
+++ evexml/forms.py
@@ -9,11 +9,15 @@ class AddAPIForm(ModelForm):
         model = APIKeyPair
         fields = '__all__'
 
-    def full_clean(self):
+    def clean(self):
+        super(AddAPIForm, self).clean()
+        self._clean()
+        return self.cleaned_data
+
+    def _clean(self):
         """Check the access mask and characters of the supplied keypair.
 
         """
-        super(AddAPIForm, self).full_clean()
         key_id = self.cleaned_data.get('key_id')
         v_code = self.cleaned_data.get('v_code')
         if not (key_id and v_code):
@@ -22,16 +26,22 @@ class AddAPIForm(ModelForm):
         api = evelink.api.API(api_key=(key_id, v_code))
         account = evelink.account.Account(api)
         try:
-            self.key_info = account.key_info().result
+            self.cleaned_data['key_info'] = account.key_info().result
         except evelink.api.APIError as error:
             self.add_error(None, error.message)
             return
 
+    def full_clean(self):
+        super(AddAPIForm, self).full_clean()
+        if not self.is_valid():
+            return
+
         self.save()
 
-        if self.key_info['type'] != 'account':
+        key_info = self.cleaned_data.get('key_info')
+        if key_info['type'] != 'account':
             self.add_error(None, 'The API key should select Character: All')
-        if self.key_info['access_mask'] != 4294967295:
+        if key_info['access_mask'] != 4294967295:
             self.add_error(None, 'The API key should have full access')
-        if self.key_info['expire_ts']:
+        if key_info['expire_ts']:
             self.add_error(None, 'The API key should have no expiry checked')
