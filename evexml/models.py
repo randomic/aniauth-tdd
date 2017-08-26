2d24f72caa0fdcd7d0188a53e78096148b6ce62e
randomic <j.a.d.mcmillan@gmail.com>
2017-08-26 19:01:51 +0100

    Wrong max_length for v_code

--- evexml/models.py
+++ evexml/models.py
@@ -3,4 +3,4 @@ from django.db import models
 
 class APIKeyPair(models.Model):
     key_id = models.IntegerField(verbose_name='keyID')
-    v_code = models.CharField(verbose_name='vCode', max_length=32)
+    v_code = models.CharField(verbose_name='vCode', max_length=64)
