3fe4f1788d82719eac70ffe0fbbbae4dbe85f00b
randomic <j.a.d.mcmillan@gmail.com>
2017-08-26 16:38:57 +0100

    Implement checks to pass tests

--- evexml/forms.py
+++ evexml/forms.py
@@ -18,14 +18,20 @@ class AddAPIForm(forms.Form):
         """
         key_id = self.cleaned_data.get('key_id')
         v_code = self.cleaned_data.get('v_code')
-        if key_id and v_code:
-            api = evelink.api.API(api_key=(key_id, v_code))
-            account = evelink.account.Account(api)
-            try:
-                key_info = account.key_info().result
-            except evelink.api.APIError as error:
-                self.add_error(None, error.message)
-                return
+        if not (key_id and v_code):
+            return
 
-            if key_info['type'] != 'account':
-                self.add_error(None, 'The API key should select Character: All')
+        api = evelink.api.API(api_key=(key_id, v_code))
+        account = evelink.account.Account(api)
+        try:
+            key_info = account.key_info().result
+        except evelink.api.APIError as error:
+            self.add_error(None, error.message)
+            return
+
+        if key_info['type'] != 'account':
+            self.add_error(None, 'The API key should select Character: All')
+        if key_info['access_mask'] != 4294967295:
+            self.add_error(None, 'The API key should have full access')
+        if key_info['expire_ts']:
+            self.add_error(None, 'The API key should have no expiry checked')
