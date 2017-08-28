962374c31e1680e5e4f6efb47ae27a7934e3dd54
randomic <j.a.d.mcmillan@gmail.com>
2017-08-28 11:51:07 +0100

    Explicitly require csrf protection

--- evexml/views.py
+++ evexml/views.py
@@ -1,4 +1,6 @@
 from django.views.generic.edit import FormView
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_protect
 from django.views.generic.base import TemplateView
 from django.urls import reverse_lazy
 
@@ -9,6 +11,10 @@ class AddAPIView(FormView):
     template_name = 'evexml/eveapi_add_form.html'
     form_class = AddAPIForm
     success_url = reverse_lazy('eveapi_added')
+
+    @method_decorator(csrf_protect)
+    def dispatch(self, *args, **kwargs):
+        return super(AddAPIView, self).dispatch(*args, **kwargs)
 
 
 class AddedAPIView(TemplateView):
